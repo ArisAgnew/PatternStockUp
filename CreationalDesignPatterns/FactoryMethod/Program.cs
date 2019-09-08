9d4c0d18eb97d7d2fa01a3951bf21ce5e303040d
ArisAgnew <aris.sektor@gmail.com>
2019-09-08 21:47:13 +0300

    CreationalDesignPatterns is full

---
+++ CreationalDesignPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreationalDesignPatterns.FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
