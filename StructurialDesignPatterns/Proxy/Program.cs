b25c60f1dcaf3ef7565c7b4a1c9f3d29153deb2d
ArisAgnew <aris.sektor@gmail.com>
2019-09-08 21:54:54 +0300

    StructurialDesignPatterns is full

---
+++ StructurialDesignPatterns/Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StructurialDesignPatterns.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
