3a70a8e117d519ad119efa1839058fd7361fba4d
ArisAgnew <aris.sektor@gmail.com>
2020-06-14 18:48:06 +0300

    Create StackToLabProbe.cs

---
+++ BehavioralDesignPatterns/Memento/Lab/StackToLabProbe.cs
@@ -0,0 +1,37 @@
+﻿using Memento.Controller;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using UsefulStuff;
+
+namespace Memento.Lab
+{
+    internal static class StackToLabProbe
+    {
+        private const int THRESHOLD = 100_000;
+        private static string RandomString => Path.GetRandomFileName().Replace(".", string.Empty);
+
+        private static MonitorHistory History { get; } = new MonitorHistory();
+
+        internal static void StackToRiseUp()
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < THRESHOLD; i++)
+            {
+                Monitor monitor = new Monitor(
+                    rnd.Next(default, int.MaxValue),
+                    rnd.Next(default, int.MaxValue),
+                    new SortedSet<string>(new string[] { RandomString }));
+
+                History.Put(monitor.Save());
+
+                $"Iteration: {i}".Depict(ConsoleColor.DarkCyan);
+            }
+
+            History.ShowHistory();
+        }
+    }
+}
