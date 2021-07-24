18d791adcd9331750d9bf46691e2e61f67756555
ArisAgnew <aris.sektor@gmail.com>
2021-07-24 23:40:24 +0300

    Create ConsoleDecoratorExtension.cs

---
+++ UsefulStuff/ConsoleDecoratorExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using static System.Console;
+using static System.ConsoleColor;
+
+namespace UsefulStuff
+{
+    public static class ConsoleDecoratorExtension
+    {
+        public static void Depict<T>(this T type,
+                                     ConsoleColor consoleColor = Gray,
+                                     bool line = default,
+                                     bool leftLine = default,
+                                     bool rightLine = default)
+            where T : IComparable<T>
+        {
+            ForegroundColor = consoleColor;
+
+            switch ((line, leftLine, rightLine))
+            {
+                case (true, false, false): Write($"{type}; "); break;
+                case (false, true, false): WriteLine($"\n{type}"); break;
+                case (false, false, true): WriteLine($"{type}\n"); break;
+                case (false, true, true): WriteLine($"\n{type}\n"); break;
+                default: WriteLine(type); break;
+            }
+
+            ResetColor();
+        }
+    }
+}
