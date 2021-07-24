c32395ab0ae3121ec835764ab622760c86a62314
ArisAgnew <aris.sektor@gmail.com>
2021-07-25 01:59:34 +0300

    Create CharacterDecorator.cs

---
+++ StructurialDesignPatterns/Decorator/AutoCarryDecorator/CharacterDecorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.AutoCarryDecorator
+{
+    public abstract class CharacterDecorator : IAutoCarryCharacter
+    {
+        protected IAutoCarryCharacter _autoCarryCharacter;
+
+        internal CharacterDecorator(IAutoCarryCharacter autoCarryCharacter)
+        {
+            _autoCarryCharacter = autoCarryCharacter 
+                ?? throw new ArgumentException(nameof(autoCarryCharacter));
+        }
+
+        public virtual IAutoCarryCharacter ImbalancedSektor() => _autoCarryCharacter.ImbalancedSektor();
+    }
+}
