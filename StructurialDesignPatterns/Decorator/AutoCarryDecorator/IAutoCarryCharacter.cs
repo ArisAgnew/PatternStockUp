46a0c09599b48cca5b8062f91f24387f63f74ee5
ArisAgnew <aris.sektor@gmail.com>
2021-07-25 01:59:29 +0300

    Create IAutoCarryCharacter.cs

---
+++ StructurialDesignPatterns/Decorator/AutoCarryDecorator/IAutoCarryCharacter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.AutoCarryDecorator
+{
+    public interface IAutoCarryCharacter : IGame
+    {
+        IAutoCarryCharacter ImbalancedSektor();
+    }
+}
