a43643b842a6cfe9a86a1a5cc4178e1e9603482b
ArisAgnew <aris.sektor@gmail.com>
2021-07-25 22:31:55 +0300

    Create EnumUtils.cs

---
+++ UsefulStuff/EnumUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using static System.Enum;
+
+namespace UsefulStuff
+{
+    public static class EnumUtils
+    {
+        private static Random _random = new();
+
+        public static T RandomEnumValue<T>() where T : Enum
+        {
+            Array values = GetValues(typeof(T));
+            return (T)values.GetValue(_random.Next(values.Length));
+        }
+    }
+}
