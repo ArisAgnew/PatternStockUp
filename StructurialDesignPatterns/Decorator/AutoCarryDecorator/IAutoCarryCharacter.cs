d3c9c0e17f59fc9e5749aa1c04cb92b65288941a
ArisAgnew <aris.sektor@gmail.com>
2021-07-26 11:47:04 +0300

    Update IAutoCarryCharacter.cs

--- StructurialDesignPatterns/Decorator/AutoCarryDecorator/IAutoCarryCharacter.cs
+++ StructurialDesignPatterns/Decorator/AutoCarryDecorator/IAutoCarryCharacter.cs
@@ -6,5 +6,6 @@ namespace Decorator.AutoCarryDecorator
 {
     public interface IAutoCarryCharacter : IGame
     {
+        IAutoCarryCharacter ImbalancedCharacter(string charName = default);
     }
 }
