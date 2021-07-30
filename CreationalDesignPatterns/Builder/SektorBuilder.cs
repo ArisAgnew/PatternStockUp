b175fac33ae90902cbe5a0a3f8cbc31a452821cd
ArisAgnew <aris.sektor@gmail.com>
2021-07-31 00:51:40 +0300

    Create SektorBuilder.cs

---
+++ CreationalDesignPatterns/Builder/SektorBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Builder
+{
+    //todo 7/31/21: Chances are, this class potentially ought to be omitted;
+    internal abstract class SektorBuilder
+    {
+        protected static Sektor sektorObject;
+
+        internal Sektor Build
+        {
+            get => sektorObject
+                ?? throw new Exception($"{nameof(Sektor)} has null value");
+            private init { }
+        }
+
+        internal abstract SektorBuilder InitAutoCarry();
+        internal abstract SektorBuilder InitTrecherousSlide();
+        internal abstract SektorBuilder InitAgility();
+        internal abstract SektorBuilder InitReward();
+        internal abstract SektorBuilder InitHighDamageOutput();        
+    }
+}
