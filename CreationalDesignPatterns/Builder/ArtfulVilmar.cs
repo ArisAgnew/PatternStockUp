f453403a8ce6e6e3a7538dba36b280528a67aba1
ArisAgnew <aris.sektor@gmail.com>
2021-07-31 00:51:27 +0300

    Create ArtfulVilmar.cs

---
+++ CreationalDesignPatterns/Builder/ArtfulVilmar.cs
@@ -0,0 +1,41 @@
+﻿namespace Builder
+{
+    internal class ArtfulVilmar : SektorBuilder
+    {
+        internal override SektorBuilder InitAgility()
+        {
+            sektorObject.Agility = "High agility";
+            return this;
+        }
+
+        internal override SektorBuilder InitAutoCarry()
+        {
+            sektorObject.AutoCarry = "Top autocarry character";
+            return this;
+        }
+
+        internal override SektorBuilder InitHighDamageOutput()
+        {
+            sektorObject.HighDamageOutput = 60D;
+            return this;
+        }
+
+        internal override SektorBuilder InitReward()
+        {
+            sektorObject.Reward = "High reward character";
+            return this;
+        }
+
+        internal override SektorBuilder InitTrecherousSlide()
+        {
+            sektorObject.TrecherousSlide = -13 & -7;
+            return this;
+        }
+
+        internal static SektorBuilder Create()
+        {
+            sektorObject = new();
+            return new ArtfulVilmar();
+        }
+    }
+}
