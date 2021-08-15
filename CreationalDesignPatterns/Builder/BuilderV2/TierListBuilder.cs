3fd1947526ae960146dc5026fb17e2aaccfa9174
ArisAgnew <aris.sektor@gmail.com>
2021-08-15 22:38:00 +0300

    Create TierListBuilder.cs

---
+++ CreationalDesignPatterns/Builder/BuilderV2/TierListBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Builder.BuilderV2
+{
+    internal abstract class TierListBuilder
+    {
+        protected internal Kharacter _kharacter;
+
+        internal Kharacter Kharacter
+        {
+            get => _kharacter ?? throw new ArgumentException();
+            private init { }
+        }
+
+        internal virtual string Game
+        {
+            get => GetGame();
+            protected private init { }
+        }
+
+        protected internal abstract string GetGame();
+
+        protected internal abstract TierListBuilder GodTier();
+        protected internal abstract TierListBuilder TopTier();
+        protected internal abstract TierListBuilder HighTier();
+        protected internal abstract TierListBuilder MidTier();
+        protected internal abstract TierListBuilder LowTier();
+        protected internal abstract TierListBuilder BottomTier();
+        protected internal abstract TierListBuilder Build();
+    }
+}
