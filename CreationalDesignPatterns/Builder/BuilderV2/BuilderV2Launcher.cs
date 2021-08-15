c7571300c998211dcbc63a557ebc26a498a9b21e
ArisAgnew <aris.sektor@gmail.com>
2021-08-15 22:37:49 +0300

    Create BuilderV2Launcher.cs

---
+++ CreationalDesignPatterns/Builder/BuilderV2/BuilderV2Launcher.cs
@@ -0,0 +1,23 @@
+﻿namespace Builder.BuilderV2
+{
+    public class BuilderV2Launcher
+    {
+        static void Main(string[] args)
+        {
+            TierListBuilder tierListBuilder = default;
+
+            #region MortalKombat9Builder
+            (tierListBuilder = MortalKombat9Builder.Initialize())
+                .GodTier()
+                .TopTier()
+                .HighTier()
+                .MidTier()
+                .LowTier()
+                .BottomTier()
+                .Build();
+
+            tierListBuilder.Kharacter.Display();
+            #endregion MortalKombat9Builder
+        }
+    }
+}
