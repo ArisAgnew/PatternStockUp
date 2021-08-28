f12204d2c183538890fcab84191683241e050b58
ArisAgnew <aris.sektor@gmail.com>
2021-08-28 15:25:08 +0300

    Create MathImpl.cs

---
+++ StructurialDesignPatterns/Proxy/MathProxy/MathImpl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Proxy.MathProxy
+{
+    enum MathOperation { None, Add, Sub, Mul, Div }
+
+    internal class MathImpl : IMath { }
+
+    internal struct MathProxyProvider
+    {
+        internal static Func<double, double, double> Release<T>(ref T self, MathOperation mathOperation)
+            where T : IMath
+        {
+            if (self is not null)
+            {
+                switch (mathOperation)
+                {
+                    case MathOperation.Add: return self.Add;
+                    case MathOperation.Sub: return self.Substract;
+                    case MathOperation.Mul: return self.Multiply;
+                    case MathOperation.Div: return self.Divide;
+                }
+            }
+            return self.NotOperable;
+        }
+    }
+}
