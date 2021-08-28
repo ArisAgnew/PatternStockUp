a394156210d1f8e0c82c5bb844a3185c5db25e62
ArisAgnew <aris.sektor@gmail.com>
2021-08-28 15:25:12 +0300

    Create IMath.cs

---
+++ StructurialDesignPatterns/Proxy/MathProxy/IMath.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using static System.Console;
+
+namespace Proxy.MathProxy
+{
+    internal interface IMath
+    {
+        Func<double, double, double> NotOperable
+        {
+            get => (a, b) => double.NaN;
+            protected set { }
+        }
+
+        Func<double, double, double> Add
+        {
+            get => (a, b) => a + b;
+            protected set { }
+        }
+
+        Func<double, double, double> Substract
+        {
+            get => (a, b) => a - b;
+            protected set { }
+        }
+
+        Func<double, double, double> Multiply
+        {
+            get => (a, b) => a * b;
+            protected set { }
+        }
+
+        Func<double, double, double> Divide
+        {
+            get
+            {
+                var result = default(Func<double, double, double>);
+                try
+                {
+                    result = (a, b) => a / b;
+                }
+                catch (DivideByZeroException)
+                {
+                    WriteLine($"Division by zero detected.");
+                }
+                return result;
+            }
+            protected set { }
+        }
+    }
+}
