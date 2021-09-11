1e825b864af66328880b23f951eec2914392702f
ArisAgnew <aris.sektor@gmail.com>
2021-09-11 23:21:12 +0300

    Create Program.cs

---
+++ Memoization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using static System.Console;
+using static System.Diagnostics.Stopwatch;
+
+namespace Memoization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> FibonacciFunc = default;
+            FibonacciFunc = n => n >= 2 ? FibonacciFunc(n - 1) + FibonacciFunc(n - 2) : n;
+
+            // Without memoization
+            var sw = StartNew();
+            for (int i = 0; i < 10_000; ++i)
+            {
+                FibonacciFunc(10);
+            }
+            WriteLine(sw.ElapsedTicks);
+
+            // Memoize function
+            FibonacciFunc = FibonacciFunc.Memoize();
+
+            // With memoization
+            sw = StartNew();
+            for (int i = 0; i < 10_000; ++i)
+            {
+                FibonacciFunc(10);
+            }
+            WriteLine(sw.ElapsedTicks);
+        }
+    }
+}
