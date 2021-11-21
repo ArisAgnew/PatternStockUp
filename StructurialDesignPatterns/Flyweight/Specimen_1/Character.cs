3671fbac600c2bd21c1f3099c54b51a4393bec94
ArisAgnew <aris.sektor@gmail.com>
2021-11-21 22:17:19 +0300

    Flyweight #1 Implementation

---
+++ StructurialDesignPatterns/Flyweight/Specimen_1/Character.cs
@@ -0,0 +1,14 @@
+﻿namespace Flyweight.Specimen_1
+{
+    internal abstract class Character
+    {
+        protected char symbol;
+        protected int width;
+        protected int height;
+        protected int ascent;
+        protected int descent;
+        protected int pointSize;
+
+        internal abstract void Display(int pointSize);
+    }
+}
---
+++ StructurialDesignPatterns/Flyweight/Specimen_1/ICharacter.cs
@@ -0,0 +1,12 @@
+﻿namespace Flyweight.Specimen_1
+{
+    internal interface ICharacter
+    {
+        internal char Symbol { get => default; set { } }
+        internal int Width { get => default; set { } }
+        internal int Height { get => default; set { } }
+        internal int Ascent { get => default; set { } }
+        internal int Descent { get => default; set { } }
+        internal int PointSize { get => default; set { } }
+    }
+}
