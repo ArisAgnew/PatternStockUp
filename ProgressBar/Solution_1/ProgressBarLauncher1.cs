9c0f798195e7109463b5e2752d758e34b6a1fa0e
ArisAgnew <aris.sektor@gmail.com>
2021-11-28 17:50:12 +0300

    Simple progress bar 1

---
+++ ProgressBar/Solution_1/ProgressBarLauncher1.cs
@@ -0,0 +1,50 @@
+﻿using static System.Console;
+using static System.Threading.Thread;
+using static ProgressBar.Solution_1.ConsoleUtility;
+
+namespace ProgressBar.Solution_1
+{
+    internal class ProgressBarLauncher1
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 100; ++i)
+            {
+                WriteProgressBar(i, true);
+                Sleep(50);
+            }
+        }
+    }
+
+    static class ConsoleUtility
+    {
+        private const char _block = (char)0x23;
+        private const string _back = "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+        private const string _twirl = "-\\|/";
+
+        public static void WriteProgressBar(int percent, bool update = false)
+        {
+            if (update)
+            {
+                Write(_back);
+            }
+            Write("[");
+            var p = (int)((percent / 10f) + .5f);
+            for (var i = 0; i < 10; ++i)
+            {
+                if (i >= p) Write(' ');
+                else Write(_block);
+            }
+            Write("] {0,3:##0}%", percent);
+        }
+
+        public static void WriteProgress(int progress, bool update = false)
+        {
+            if (update)
+            {
+                Write("\b");
+            }
+            Write(_twirl[progress % _twirl.Length]);
+        }
+    }
+}
