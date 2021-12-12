32949172bd6825a6d9e1ec0bce9bb612fb3dab8a
ArisAgnew <aris.sektor@gmail.com>
2021-12-12 21:11:12 +0300

    Update Program.cs

--- MiscellaneousStuff/SortAlgorithms/QuickSort/Program.cs
+++ MiscellaneousStuff/SortAlgorithms/QuickSort/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 
 using static System.Console;
@@ -10,6 +11,7 @@ namespace MiscellaneousStuff.SortAlgorithms.QuickSort
     class Program
     {
         private const char SPACE = ' ';
+        private static Stopwatch stopwatch = new();
 
         static void Main(string[] args)
         {
@@ -25,19 +27,42 @@ namespace MiscellaneousStuff.SortAlgorithms.QuickSort
             #endregion
 
             #region Recursive sorting
-            WriteLine("\nRecursive Sorting:");
-            PrimaryQuickSort.ByUsingRecursive.SortRecursive(array).ToImmutableList().ForEach(n => Write($"{n}{SPACE}"));
+            WriteLine("\n\nRecursive Sorting:");
+            {
+                stopwatch.Start();
+                PrimaryQuickSort.ByUsingRecursive.SortRecursive(array)
+                    .ToImmutableList()
+                    .ForEach(n => Write($"{n}{SPACE}"));
+                stopwatch.Stop();
+                WriteLine($"\nRecursive Sorting --> {Elapsed(stopwatch)}");
+            }
             #endregion
 
             #region Iterative Sorting
             WriteLine("\nIterative Sorting:");
-            PrimaryQuickSort.SortIterative(array).ToImmutableList().ForEach(n => Write($"{n}{SPACE}"));
+            {
+                stopwatch.Start();
+                EnumerableQuickSort.Sort(array)
+                    .ToImmutableList()
+                    .ForEach(n => Write($"{n}{SPACE}"));
+                stopwatch.Stop();
+                WriteLine($"\nIterative Sorting --> {Elapsed(stopwatch)}");
+            }
             #endregion
 
             #region Sorting by means of using IEnumerable LINQ methods
             WriteLine("\nEnumerable Sorting:");
-            EnumerableQuickSort.Sort(array).ToImmutableList().ForEach(n => Write($"{n}{SPACE}"));
+            {
+                stopwatch.Start();
+                EnumerableQuickSort.Sort(array)
+                    .ToImmutableList()
+                    .ForEach(n => Write($"{n}{SPACE}"));
+                stopwatch.Stop();
+                WriteLine($"\nLookup quick sorting --> {Elapsed(stopwatch)}");
+            }
             #endregion
         }
+
+        static string Elapsed(in Stopwatch stopwatch) => stopwatch.Elapsed.ToString(@"hh\:mm\:ss\.fffffff");
     }
 }
