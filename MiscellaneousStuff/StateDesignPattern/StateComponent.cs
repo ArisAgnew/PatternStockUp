a9d8e1d2c57aa6ee3ad1b4ab9c9bf04651da93f6
ArisAgnew <aris.sektor@gmail.com>
2022-02-24 23:34:32 +0300

    LikeDislike State Design Pattern
    Initial entities have been written.

---
+++ MiscellaneousStuff/StateDesignPattern/StateComponent.cs
@@ -0,0 +1,24 @@
+﻿namespace StateDesignPattern
+{
+    internal sealed class StateComponent : IState
+    {
+        private (ILike like, IDislike dislike) _state = default;
+
+        internal StateComponent((ILike, IDislike) state) => _state = state;
+
+        internal StateComponent(in int likes, in int dislikes, (ILike, IDislike) state) : this(state) =>
+            (Likes, Dislikes) = (likes, dislikes);
+
+        internal int Likes { get; set; }
+        internal int Dislikes { get; set; }
+
+        internal StateComponent ChangeState((ILike, IDislike) state)
+        {
+            _state = state;
+            return this;
+        }
+
+        internal IState DoLike() => _state.like.DoLike(this);
+        internal IState DoDislike() => _state.dislike.DoDislike(this);
+    }
+}
